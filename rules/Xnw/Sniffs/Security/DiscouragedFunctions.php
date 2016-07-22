13f6a60877e16bf281e6d891a1ae799eb4e6c1c8
mr2longly <mr2longly@gmail.com>
2016-07-22 15:30:38 +0800

    add xnw php-code-sniffer

---
+++ rules/Xnw/Sniffs/Security/DiscouragedFunctions.php
@@ -0,0 +1,35 @@
+<?php
+namespace Xnw\Sniffs\Security;
+
+use Generic_Sniffs_PHP_ForbiddenFunctionsSniff;
+
+class DiscouragedFunctionSniff extends Generic_Sniffs_PHP_ForbiddenFunctionsSniff
+{
+    /**
+     * If true, an error will be thrown; otherwise a warning.
+     *
+     * @var bool
+     */
+    public $error = false;
+
+    /**
+     * If true, forbidden functions will be considered regular expressions.
+     *
+     * @var bool
+     */
+    protected $patternMatch = true;
+
+    /**
+     * A list of forbidden functions with their alternatives.
+     *
+     * The value is NULL if no alternative exists. IE, the
+     * function should just not be used.
+     *
+     * @var array(string => string|null)
+     */
+    public $forbiddenFunctions = array(
+        '^is_dir' => null,
+        '^is_file$' => null,
+        '^pathinfo$' => null,
+    );
+}
